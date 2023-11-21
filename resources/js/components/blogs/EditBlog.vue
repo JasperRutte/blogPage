039f81d12cfb42f608166a7c96a352049fb82e3e
JasperRutte <jasperrutte@gmail.com>
2023-11-21 17:01:10 +0100

    working on editing blogposts

--- resources/js/components/blogs/EditBlog.vue
+++ resources/js/components/blogs/EditBlog.vue
@@ -1,6 +1,26 @@
 <template>
-    <p>twdewadwadaw</p>
+    <input v-model="blogPost.id" >
 </template>
 <script>
+export default {
+    data(){
+        return {
+            blogPost: [],
+            hasLoaded: false
+        }
+    },
+
+    mounted() {
+        axios.get(`/api/show/`+this.$route.params.id)
+            .then(response => {
+                this.blogPost = response.data.blog;
+                this.hasLoaded = true
+                console.log(this.blogPost)
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    },
+}
 
 </script>
